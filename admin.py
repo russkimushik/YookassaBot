6a4c9940b6bd18d4e7ce5995225baca91e1d2316
Степан Саблин <sablin.stepan.2010@gmail.com>
2025-05-31 18:34:44 +0300

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,19 @@
+import aiosqlite
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery
+from aiogram.filters import Filter, Command
+from aiogram.fsm.context import FSMContext
+
+import app.keyboards as kb
+from app.database.requests import get_users
+
+admin = Router()
+
+class Admin(Filter):
+    async def __call__(self, message: Message):
+        return message.from_user.id in [2066791910]
+
+@admin.message(Admin(), Command('send_mes'))
+async def newsletter(message: Message, state: FSMContext):
+    await state.set_state(Send_Mes.message)
+    await message.answer('Введитие сообщение для рассылки')
